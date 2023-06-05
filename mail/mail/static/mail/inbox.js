a6234b4f3f674a95bd19e108b690d7ee68d1190d
furbano-absolute <furbano@absoluteinvest.com.br>
2023-06-05 09:48:56 -0300

    changed js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  // document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-submit').addEventListener('click', submit_compose);
+  document.querySelector('#inbox').addEventListener('click', mailbox_view);
 
   // By default, load the inbox
-  load_mailbox('inbox');
+  // load_mailbox('inbox');
 });
 
 function compose_email() {
@@ -30,4 +32,40 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function submit_compose() {
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients'),
+        subject: document.querySelector('#compose-subject'),
+        body: document.querySelector('#compose-body')
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+
+}
+
+function mailbox_view() {
+
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+      const mailbox = document.createElement('div');
+      mailbox.innerHTML = 'This is the content of the div.';
+      document.querySelector('#email-views').innerHTML = 'This is the content of the div.'
+  });
+
 }
